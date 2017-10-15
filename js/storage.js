d1ae3e366e6076dee1e83d34396a03ffc28cc993
daisy1754 <d-part@hotmail.co.jp>
2017-10-15 11:04:22 -0700

    Implement index page to show all diagrams

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,7 @@
 const KEY_ITEM_IDS = "itemIds";
 const KEY_LAST_ITEM_ID = "lastItemId";
 const KEY_ITEM_PREFIX = "item_";
+const KEY_TITLE_PREFIX = "item_title_";
 
 function _removeElement(array, element) {
   var i = array.indexOf(element);
@@ -33,4 +34,21 @@ function saveDiagram(id, text) {
 
 function setLastItemId(id) {
   localStorage.setItem(KEY_LAST_ITEM_ID, id);
+}
+
+// Returns array of diagram (id, title, content)
+function getDiagrams() {
+  let itemIds = localStorage.getItem(KEY_ITEM_IDS);
+  if (!itemIds) {
+    return [];
+  }
+  var result = [];
+  itemIds = itemIds.split(",");
+  for (var i = itemIds.length - 1; i >= 0; i--) {
+    const id = itemIds[i];
+    result.push({id: id,
+      title: localStorage.getItem(KEY_TITLE_PREFIX + id),
+      content: localStorage.getItem(KEY_ITEM_PREFIX + id)});
+  }
+  return result;
 }
